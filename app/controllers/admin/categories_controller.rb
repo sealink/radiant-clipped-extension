4188b0544eac0d949d2607d8cec4635ff4651d5d
Stefan <stefanste@gmail.com>
2013-02-11 15:52:34 +1030

    Fixes changing category name bug due to assets not being moved to new destination

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,8 +1,17 @@
 class Admin::CategoriesController < Admin::ResourceController
-
   def index
     @categories = Category.all
     render 'index'
+  end
+
+  def update
+    if params[:id]
+      @category = Category.find(params[:id])
+      old_path = Rails.root.to_s << "/public/assets/#{@category.name.downcase.parameterize}"
+      @category.update_attributes(params[:category])
+      move_assets_to_new_path(old_path)
+    end
+    redirect_to admin_categories_path
   end
 
   def destroy
@@ -13,4 +22,15 @@ class Admin::CategoriesController < Admin::ResourceController
     redirect_to admin_categories_path
   end
 
+  private
+
+  def move_assets_to_new_path(old_path)
+    src = old_path + '/.'  # All files within the old_path directory
+    dest = Rails.root.to_s << "/public/assets/#{@category.name.downcase.parameterize}"
+    FileUtils.mkdir_p(dest)
+    FileUtils.cp_r(src, dest)
+    FileUtils.rm_r(old_path)
+  end
+
 end
+
