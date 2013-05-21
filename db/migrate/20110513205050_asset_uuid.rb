17e84bcfb91d2462f6f374e619e20bddb5d1d1b3
Michael Noack <michael@noack.com.au>
2013-05-21 09:59:42 +0930

    Do it properly - Moved 2 added migration to end, and fixed the problem migration by implementing without "save"

--- db/migrate/20110513205050_asset_uuid.rb
+++ db/migrate/20110513205050_asset_uuid.rb
@@ -2,7 +2,9 @@ class AssetUuid < ActiveRecord::Migration
   def self.up
     add_column :assets, :uuid, :string
     Asset.reset_column_information
-    Asset.all.each { |a| a.save! }    # triggers assign_uuid
+    Asset.scoped(:conditions => "uuid IS NULL").each do |a|
+      Asset.update_all({:uuid => UUIDTools::UUID.timestamp_create.to_s}, {:id => a.id})
+    end
   end
 
   def self.down
