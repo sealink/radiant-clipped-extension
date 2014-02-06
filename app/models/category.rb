60dea59a1cc10fad603de2f0227d181cb240d4e1
Stefan Kojic <stefan.kojic@sealink.com.au>
2014-02-06 15:27:31 +1030

    Don't allow page asset category to be changed or removed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,15 @@
 class Category < ActiveRecord::Base
 
   DEFAULT_CATEGORY = 'default'
+  PAGE_ASSET_CATEGORY = 'Page Asset'
+  PROTECTED_CATEGORIES = [DEFAULT_CATEGORY, PAGE_ASSET_CATEGORY]
   before_destroy :reattach_assets_if_destroyable
   before_update :updatable?
 
   named_scope :ordered, :order => :name
 
   def reattach_assets_if_destroyable
-    raise ActiveRecord::ActiveRecordError, "You cannot delete the default category." if name == DEFAULT_CATEGORY
+    raise ActiveRecord::ActiveRecordError, "You cannot delete this category." if PROTECTED_CATEGORIES.include?(name)
 
     Asset.all(:conditions => ["category_id = ?", id]).each do |asset|
       new_attachment = File.new(asset.asset.path)
@@ -22,7 +24,7 @@ class Category < ActiveRecord::Base
   end
 
   def updatable?
-    raise ActiveRecord::ActiveRecordError, "You cannot modify the default category." if name_was == DEFAULT_CATEGORY
+    raise ActiveRecord::ActiveRecordError, "You cannot modify this category." if PROTECTED_CATEGORIES.include?(name_was)
   end
 
 end
